165ca70a9bf6cc375b97391d764ae1a686ce506b
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-11 10:33:12 +0200

    added methods to EmployeeDetailService

--- src/main/java/com/demirtag/intranet/service/EmployeeDetailService.java
+++ src/main/java/com/demirtag/intranet/service/EmployeeDetailService.java
@@ -1,8 +1,11 @@
 package com.demirtag.intranet.service;
 
+import com.demirtag.intranet.model.EmployeeDetail;
 import com.demirtag.intranet.repository.EmployeeDetailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class EmployeeDetailService {
@@ -12,5 +15,27 @@ public class EmployeeDetailService {
     @Autowired
     public EmployeeDetailService(EmployeeDetailRepository employeeDetailRepository) {
         this.employeeDetailRepository = employeeDetailRepository;
+    }
+
+
+    public EmployeeDetail createEmployeeDetail(EmployeeDetail employeeDetail) {
+        return employeeDetailRepository.save(employeeDetail);
+    }
+
+    public Optional<EmployeeDetail> getEmployeeDetail(Long id) {
+        return employeeDetailRepository.findById(id);
+    }
+
+    public EmployeeDetail updateEmployeeDetail(EmployeeDetail employeeDetail) {
+        return employeeDetailRepository.save(employeeDetail);
+    }
+
+    public boolean deleteEmployeeDetail(Long id) {
+        Optional<EmployeeDetail> employeeDetail = employeeDetailRepository.findById(id);
+        if (employeeDetail.isPresent()) {
+            employeeDetailRepository.delete(employeeDetail.get());
+            return true;
+        }
+        return false;
     }
 }
