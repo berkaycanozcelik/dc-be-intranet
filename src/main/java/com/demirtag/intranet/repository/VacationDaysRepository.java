5c1da878ba7c18cb3d1ae65fae8f911fa7a1e906
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-11 10:24:17 +0200

    created VacationDaysRepository

---
+++ src/main/java/com/demirtag/intranet/repository/VacationDaysRepository.java
@@ -0,0 +1,7 @@
+package com.demirtag.intranet.repository;
+
+import com.demirtag.intranet.model.VacationDays;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VacationDaysRepository extends JpaRepository<VacationDays, Long> {
+}
