b239adb2129198fb17f67a40bd843cf0401ccf97
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-17 11:37:14 +0200

    adjusted UserRepository

--- src/main/java/com/demirtag/intranet/repository/UserRepository.java
+++ src/main/java/com/demirtag/intranet/repository/UserRepository.java
@@ -3,5 +3,10 @@ package com.demirtag.intranet.repository;
 import com.demirtag.intranet.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
 }
