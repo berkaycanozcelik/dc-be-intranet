4e82ecab5d0e126a2a3a3084a7075f89c90ce52f
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-20 17:04:37 +0200

    added userDetails to user when userDetails is created

--- src/main/java/com/demirtag/intranet/controller/UserDetailController.java
+++ src/main/java/com/demirtag/intranet/controller/UserDetailController.java
@@ -1,7 +1,10 @@
 package com.demirtag.intranet.controller;
 
+import com.demirtag.intranet.model.User;
 import com.demirtag.intranet.model.UserDetail;
+import com.demirtag.intranet.repository.UserRepository;
 import com.demirtag.intranet.service.UserDetailService;
+import com.demirtag.intranet.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,15 +17,28 @@ import java.util.Optional;
 public class UserDetailController {
 
     private final UserDetailService userDetailService;
+    private final UserService userService;
+    private final UserRepository userRepository;
 
     @Autowired
-    public UserDetailController(UserDetailService userDetailService) {
+    public UserDetailController(UserDetailService userDetailService, UserService userService, UserRepository userRepository) {
         this.userDetailService = userDetailService;
+        this.userService = userService;
+        this.userRepository = userRepository;
     }
 
     @PostMapping("/create")
     public ResponseEntity<UserDetail> createUserDetail(@RequestBody UserDetail userDetail) {
         UserDetail savedUserDetail = userDetailService.createUserDetail(userDetail);
+        return ResponseEntity.ok(savedUserDetail);
+    }
+
+    @PostMapping("/create/{id}")
+    public ResponseEntity<UserDetail> createUserDetailForUser(@RequestBody UserDetail userDetail, @PathVariable Long id) {
+        UserDetail savedUserDetail = userDetailService.createUserDetail(userDetail);
+        User user = userService.getUserById(id);
+        user.setUserDetail(savedUserDetail);
+        userRepository.save(user);
         return ResponseEntity.ok(savedUserDetail);
     }
 
