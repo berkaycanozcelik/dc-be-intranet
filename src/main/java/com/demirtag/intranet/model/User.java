66b105e2929081cf697e95b2ee16db8432fa30c2
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-20 15:01:24 +0200

    added JsonIgnore to unnecessary fields

--- src/main/java/com/demirtag/intranet/model/User.java
+++ src/main/java/com/demirtag/intranet/model/User.java
@@ -46,36 +46,43 @@ public class User implements UserDetails {
             , cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
     private List<Holiday> holidays;
 
+    @JsonIgnore
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
+    @JsonIgnore
     @Override
     public String getPassword() {
         return password;
     }
 
+    @JsonIgnore
     @Override
     public String getUsername() {
         return email;
     }
 
+    @JsonIgnore
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
 
+    @JsonIgnore
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
 
+    @JsonIgnore
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
+    @JsonIgnore
     @Override
     public boolean isEnabled() {
         return true;
