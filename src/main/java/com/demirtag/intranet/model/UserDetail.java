b3af8efaa801ed16b2934bb21e77dad36dc1a25a
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-21 12:59:40 +0200

    updated UserDetail model

--- src/main/java/com/demirtag/intranet/model/UserDetail.java
+++ src/main/java/com/demirtag/intranet/model/UserDetail.java
@@ -1,7 +1,15 @@
 package com.demirtag.intranet.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "user_detail")
 public class UserDetail {
@@ -14,70 +22,19 @@ public class UserDetail {
 
     private String phoneNumber;
 
-    private String job;
+    private String birthday;
+
+    private byte[] imageData;
+
+    private String title;
+
+    private String Manager;
+
+    private String Team;
 
     @OneToOne(mappedBy = "userDetail"
             , cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
     private User user;
 
-    public UserDetail() {
-    }
 
-    public UserDetail(String address, String phoneNumber, String job, User user) {
-        this.address = address;
-        this.phoneNumber = phoneNumber;
-        this.job = job;
-        this.user = user;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getJob() {
-        return job;
-    }
-
-    public void setJob(String job) {
-        this.job = job;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    @Override
-    public String toString() {
-        return "UserDetail{" +
-                "id=" + id +
-                ", address='" + address + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", job='" + job + '\'' +
-                ", user=" + user +
-                '}';
-    }
 }
