7f2682a83bf04883cb2d912fb0f1c4528ea73613
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-26 14:48:27 +0200

    added addHoliday method to User class

--- src/main/java/com/demirtag/intranet/model/User.java
+++ src/main/java/com/demirtag/intranet/model/User.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -45,6 +46,16 @@ public class User implements UserDetails {
     @OneToMany(mappedBy = "user"
             , cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
     private List<Holiday> holidays;
+
+
+    public void addHoliday(Holiday holiday){
+        if (holidays == null){
+            holidays = new ArrayList<>();
+        }
+        holidays.add(holiday);
+
+        holiday.setUser(this);
+    }
 
     @JsonIgnore
     @Override
